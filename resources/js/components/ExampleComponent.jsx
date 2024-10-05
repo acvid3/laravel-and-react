c5d4702aba2a0d3d148d8607acd2e780a981fede
acvid3 <acvid3@gmail.com>
2024-10-05 21:00:51 +0300

    first commit

---
+++ resources/js/components/ExampleComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ExampleComponent = () => {
+    return <h1>Hello, Laravel + React!</h1>;
+};
+
+export default ExampleComponent;
---
+++ resources/views/main.blade.php
@@ -0,0 +1,4 @@
+<div id="app"></div>
+
+@viteReactRefresh
+@vite('resources/js/app.jsx')
